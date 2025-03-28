9f721b6298da1990c1b06e826f7df8707f2be419
Chris <christian.borza@students.codeinstitute.net>
2025-03-28 10:58:00 +0100

    Fix: all/ endpoint for AllPosts added

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import PostList, PostDetail, PostFeed, AllPosts
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),
-    path('feed/', views.PostFeed.as_view()),
-    path('all/', views.AllPosts.as_view(), name='all-posts'),
+    path('', PostList.as_view(), name='create-post'),
+    path('<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('feed/', PostFeed.as_view(), name='post-feed'),
+    path('all/', AllPosts.as_view(), name='all-posts'),
 ]
